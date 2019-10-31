a2587ff5e43d8c7183c2765c66f69c6a3e1a12fd
Asif Shahriar <a.shahriar1999@gmail.com>
2019-11-01 02:18:08 +0600

    added profile model

--- main/models.py
+++ main/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=244, blank=True, null=True)
+    gender = models.CharField(max_length=244, blank=True, null=True)
+    profession = models.CharField(max_length=244, blank=True, null=True)
+    blood = models.CharField(max_length=244, blank=True, null=True)
+    cell = models.CharField(max_length=244, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
